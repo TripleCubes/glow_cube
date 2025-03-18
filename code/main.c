81b071c70c8c5dc657f8509b07bff1ee91d1c806
TripleCubes <113813506+TripleCubes@users.noreply.github.com>
2025-03-18 14:56:15 +0700

    Add .gitattributes

--- code/main.c
+++ code/main.c
@@ -4,14 +4,6 @@
 #include "file.h"
 
 int main(void) {
-	Str str;
-	HANDLE_MAIN(str_cap_new(&str, 4096));
-
-	HANDLE_MAIN(file_read(&str, "shader/main_v.glsl"));
-
-	str_print(&str);
-	str_release(&str);
-
 	printf("reached end of main()\n");
 
 	return 0;
