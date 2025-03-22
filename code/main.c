8d0ffb5031df4e378eba985c309e19ccfe371201
TripleCubes <113813506+TripleCubes@users.noreply.github.com>
2025-03-22 19:53:44 +0700

    window now unresizable

--- code/main.c
+++ code/main.c
@@ -14,6 +14,7 @@ static Result init() {
 	glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
 	glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
 	glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+	glfwWindowHint(GLFW_RESIZABLE, GLFW_FALSE);
 
 	glfw_window = glfwCreateWindow(
 		INIT_WINDOW_W,
